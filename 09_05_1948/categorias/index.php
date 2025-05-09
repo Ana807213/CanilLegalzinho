ee29ab3e504b105120f6db1c9c328871e55e4294
Ana807213 <anapsmorais07@gmail.com>
2025-05-09 19:49:42 -0300

    09/05
    Ajuste na estrutura do projeto

---
+++ 09_05_1948/categorias/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabela de Categorias</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: center;
+            background-color: #f4f4f4;
+        }
+    </style>
+</head>
+<body>
+    <h1>Cadastro de Categorias</h1>
+    <button class="btn">Adicionar Produto</button>
+    <table>
+        <thead>
+            <tr>
+                <th>Foto</th>
+                <th>Categoria</th>
+                <th>Produto</th>
+                <th>Quantidade</th>
+                <th>Valor Unitário</th>
+                <th>Valor Total</th>
+            </tr>
+        </thead>
+    </table>
+</body>
+</html>
